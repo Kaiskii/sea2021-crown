570faa55c2b29cc7f9d4368445ebb7199a636548
Kaiskii <24761071+Kaiskii@users.noreply.github.com>
2021-09-18 20:02:54 +0800

    feat: able to change between characters

---
+++ Assets/Scripts/Kai/ActivePlayerManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivePlayerManager : MonoBehaviour {
+  private static ActivePlayerManager _instance;
+
+  public static ActivePlayerManager Instance { get { return _instance; } }
+
+  public GameObject ActivePlayer;
+
+  private void Awake() {
+    if (_instance != null && _instance != this)
+    {
+      Destroy(this.gameObject);
+    } else {
+      _instance = this;
+    }
+  }
+}
--- Assets/Scripts/Kai/PlayerThrow.cs
+++ Assets/Scripts/Kai/PlayerThrow.cs
@@ -80,6 +80,14 @@ public class PlayerThrow : MonoBehaviour {
         physicsCrown.GetComponent<Rigidbody2D>().AddTorque(torqueStrength);
         physicsCrown.GetComponent<Rigidbody2D>().AddForce(multipliedVector);
         this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        // Setting Properties to become an NPC
+        this.GetComponent<PlayerReceive>().enabled = true;
+        this.GetComponent<CharacterMovement>().enabled = false;
+
+        foreach (Transform tr in this.transform.parent) {
+          tr.gameObject.layer = LayerMask.NameToLayer("OtherNPC");
+        }
       }
 
       mouseHoldTimer = mouseHoldTime;
@@ -88,17 +96,10 @@ public class PlayerThrow : MonoBehaviour {
   }
 
   private void OnTriggerExit2D(Collider2D other) {
-    if (other.CompareTag("CrownPhysics")) {
-      // Setting Properties to become an NPC
-      this.GetComponent<PlayerReceive>().enabled = true;
-      this.GetComponent<CharacterMovement>().enabled = false;
-
-      foreach (Transform tr in this.transform.parent) {
-        tr.gameObject.layer = LayerMask.NameToLayer("OtherNPC");
-      }
-
+    if (other.CompareTag("CrownPhysics") && ActivePlayerManager.Instance.ActivePlayer == this.gameObject) {
+      Debug.Log("EEEEE");
+      ActivePlayerManager.Instance.ActivePlayer = null;
       this.enabled = false;
     }
   }
-
 }
