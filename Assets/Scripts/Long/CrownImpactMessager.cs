a162b0b8a0cd1d608e94f4016a74aacd99c10f8d
Long YM <lymprivateacc@gmail.com>
2021-09-18 22:04:43 +0800

    feat: Added rotation to crown impact

--- Assets/Scripts/Long/CrownImpactMessager.cs
+++ Assets/Scripts/Long/CrownImpactMessager.cs
@@ -3,12 +3,14 @@ using System;
 
 public class CrownImpactMessager : MonoBehaviour
 {
+  [SerializeField] string hitSFX = "CrownLand";
+  [SerializeField] string hitParticle = "FireLine";
+
   void OnCollisionEnter2D(Collision2D col)
   {
-    SoundManager.Instance?.PlayRandomPitch("CrownLand");
-    ScreenShake.Instance?.ShakeCamera(0.2f,0.2f,0.2f);
-    ParticleManager.Instance?.CreateParticle("CrownPuff",col.contacts[0].point);
-
+    SoundManager.Instance?.PlayRandomPitch(hitSFX,0.6f,0.9f);
+    ScreenShake.Instance?.ShakeCamera(3f,0.2f,2f);
+    ParticleManager.Instance?.CreateParticle(hitParticle,col.GetContact(0).point,col.GetContact(0).normal);
     CrownEventListener.Instance?.OnCrownImpact(this.gameObject,EventArgs.Empty);
   }
 }
