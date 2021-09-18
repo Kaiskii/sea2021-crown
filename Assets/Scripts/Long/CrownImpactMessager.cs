c4aa885cbe923d043240809cdf989ffcec2bb3c6
Long YM <lymprivateacc@gmail.com>
2021-09-19 02:55:34 +0800

    feat: Added title screen

--- Assets/Scripts/Long/CrownImpactMessager.cs
+++ Assets/Scripts/Long/CrownImpactMessager.cs
@@ -5,11 +5,12 @@ public class CrownImpactMessager : MonoBehaviour
 {
   [SerializeField] string hitSFX = "CrownLand";
   [SerializeField] string hitParticle = "FireLine";
+  [SerializeField] Vector3 cameraShakeParams = new Vector3(5f,0.2f,3f);
 
   void OnCollisionEnter2D(Collision2D col)
   {
     SoundManager.Instance?.PlayRandomPitch(hitSFX,0.6f,0.9f);
-    ScreenShake.Instance?.ShakeCamera(5f,0.2f,3f);
+    ScreenShake.Instance?.ShakeCamera(cameraShakeParams.x,cameraShakeParams.y,cameraShakeParams.z);
     ParticleManager.Instance?.CreateParticle(hitParticle,transform.position,col.GetContact(0).normal);
     CrownEventListener.Instance?.OnCrownImpact(this.gameObject,EventArgs.Empty);
   }
